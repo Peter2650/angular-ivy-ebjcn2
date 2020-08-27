c59d7919d4933af6a3fe708c0194b4c4cf963e8d
Peter2650 <peter@heick.dk>
2020-08-27 17:26:07 +0200

    5 files modified

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member.component.css']
 })
 export class MemberComponent implements OnInit {
+  
+  line = [
+    'It seems that named router outlet only works with full path:',
+    '<a [routerLink]="[\'/home/member\', {outlets: {right: [\'edit\', member.id]}}]"></a>',
+    'I would expect that it also would work with a relative path:',
+    '<a [routerLink]="[{outlets: {right: [\'edit\', member.id]}}]"></a>',
+  ];
 
   constructor() { }
 
